b71acfe2d8da50e02aad689c8de9720b7ad800c2
Richard Bouaro <richard.bouaro@amalitech.com>
2025-07-16 10:58:00 +0000

    chore: initial setup

---
+++ src/main/java/com/rbouaro/ragsystem/RagSystemApplication.java
@@ -0,0 +1,13 @@
+package com.rbouaro.ragsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RagSystemApplication.class, args);
+    }
+
+}
